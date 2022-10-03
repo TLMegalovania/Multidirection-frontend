ed9734658f5504ddb8c37279333c84f9f7109e31
Thallium Megalovania <tlmegalovania@qq.com>
2022-10-03 16:24:55 +0800

    thank you css
    Signed-off-by: Thallium Megalovania <tlmegalovania@qq.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,48 @@
 <script lang="ts">
-  let deg = 0
-  let shouldRotate = false
-  const refresh = (time:number)=>{
-    if(shouldRotate) deg+=1;
-    window.requestAnimationFrame(refresh)
-  }
-  window.requestAnimationFrame(refresh)
-  $:document.documentElement.style.setProperty('--bg-image',`linear-gradient(${deg}deg,#efefef, #101010)`)
-</script>
-<!-- <main style:--bg-image='linear-gradient(90deg,#efefef, #101010)'> -->
-<h1>
-多向棋
-</h1>
+  let deg = 90;
+  let shouldTrans = false;
+  let trans: number;
+  const transColor = () => {
+    shouldTrans = true;
+    const _change = () => {
+      if (deg == 90) deg = 10;
+      else deg = 90;
+    };
+    _change();
+    trans = setInterval(_change, 4500);
+  };
+  const stopTrans = () => {
+    shouldTrans = false;
+    clearInterval(trans);
+  };
+  const rootStyle = document.documentElement.style;
 
-<button on:click="{()=>shouldRotate=!shouldRotate}">匹配</button>
-<br>
+  $: rootStyle.setProperty("--bg-color", `hsl(0,0%,${deg}%)`);
+  $: rootStyle.setProperty("--fg-color", `hsl(0,0%,${100 - deg}%)`);
+</script>
+
+<!-- <main style:--bg-image='linear-gradient(90deg,#efefef, #101010)'> -->
+<h1>多向棋</h1>
+
+<button on:click={shouldTrans ? stopTrans : transColor}>
+  {#if shouldTrans}
+    取消
+  {:else}
+    匹配
+  {/if}
+</button>
+<br />
 <button>帮助</button>
-<svelte:body style="--bg-image:linear-gradient(#efefef, #101010)" />
 
 <style>
-  h1{
+  h1 {
     margin: 20vh auto 0;
     line-height: 1em;
   }
-  h1+button{
+  h1 + button {
     margin-top: 30vh;
   }
-  button{
+  button {
     width: 16vw;
     margin-top: 8vh;
   }
